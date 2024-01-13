a0c15ae76619c0d85919346226248d0255a6dae3
MahBadran93 <mahmoud.s.badran@gmail.com>
2024-01-13 15:40:04 +0200

    add bastion host on a public subnet in my vpc to access private subnet instances to be able to install kafka using ansible automation

---
+++ terraforms/bastion-host.tf
@@ -0,0 +1,35 @@
+# Bastion Host Security Group
+# add a security group for the bastion host to allow SSH from my IP only
+# the bastion host will be used to connect to the private instances in the private subnet to be able to setup kafka via ansible 
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Security group for the bastion host"
+  vpc_id      = aws_vpc.main.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["139.190.184.234/32"] # Allow SSH only from my IP - mahmoud local machine 
+  }
+
+  # Add any other necessary rules...
+}
+
+# Bastion Host EC2 Instance
+resource "aws_instance" "bastion_host" {
+  ami           = "ami-0005e0cfe09cc9050"  # Replace with the AMI ID for your desired OS
+  instance_type = "t2.medium"            # Choose an appropriate instance type
+
+  subnet_id               = element(aws_subnet.public_subnets[*].id, 0)
+#   key_name                = "your_key_pair_name"  # Replace with your key pair name
+  vpc_security_group_ids  = [aws_security_group.bastion_sg.id]
+
+
+  tags = {
+    Name = "bastion-host"
+  }
+}
+
+output "bastion_host_public_ip" {
+  value = aws_instance.bastion_host.public_ip
+}
